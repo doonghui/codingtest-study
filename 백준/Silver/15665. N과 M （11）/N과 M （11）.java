0ee85cf2854631420281df62017325b41394fa4e
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-04-20 23:19:25 +0900

    [Silver II] Title: N과 M (11), Time: 400 ms, Memory: 34292 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -1,55 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n, m;
-    public static int[] arr, nums;
-    //public static boolean[] visit; // 방문 여부
+
+
+    static int[] arr;
+    static int[] sol;
     public static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arr = new int[m];
-        //visit = new boolean[n + 1];
-
-        nums = new int[n + 1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(nums);
-
-        dfs(0);
-        System.out.println(sb);
-    }
-
-    public static void dfs(int cnt) {
-
-        if (cnt == m) {
-            for (int val : arr) {
+    static void Sol(int n, int m, int L) {
+        int visit = 0;
+        if (L == m) {
+            for (int val : sol) {
                 sb.append(val).append(" ");
             }
             sb.append("\n");
             return;
+        } else {
+            for (int i = 0; i <= n; i++) {
+                if (visit != arr[i]) {
+                    sol[L] = arr[i];
+                    visit = arr[i];
+                    Sol(n, m, L + 1);
+                }
+
+            }
         }
 
-        int before = 0;
-        for (int i = 0; i <= n; i++) {
-            if (before != nums[i]) {
-                arr[cnt] = nums[i];
-                before = nums[i];
-                dfs(cnt + 1);
-            }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[n+1];
+        sol = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+            Arrays.sort(arr);
+            Sol(n, m, 0);
+        System.out.println(sb);
 
         }
     }
-}
+
