50e91aef23290c12e4c07056e19c507dec8b857b
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-07-15 17:24:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.36 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,19 +3,18 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 class Solution {
-    static ArrayList<Integer> tmp;
      static int[] solution(String[] operations) {
             int[] answer = {};
             answer = new int[2];
 
+            // 우선순위 작은 값부터 나온다.
             PriorityQueue<Integer> pQ = new PriorityQueue<>();
 
+         
             for(String s : operations) {
-                if(s.equals("D -1")) {
+                if(s.equals("D -1")) {          // 최솟값은 그대로 poll() 로 나오게 한다.
                     if(!pQ.isEmpty())
                         pQ.poll();
-
-
 
                 } else if(s.equals("D 1")) {
                     ArrayList<Integer> arr = new ArrayList<>();
@@ -30,7 +29,7 @@ class Solution {
 
                 } else {
                     int num;
-                    if(s.charAt(1) == '-') {
+                    if(s.charAt(1) == '-') {        // 음수일 경우
                         num = Integer.parseInt(s.substring(1));
                     } else{
                         num = Integer.parseInt(s.substring(2));
@@ -38,16 +37,15 @@ class Solution {
                     pQ.add(num);
                 }
             }
-            tmp = new ArrayList<>();
+         
             ArrayList<Integer> arr2 = new ArrayList<>();
             if(!pQ.isEmpty()){
                 while(!pQ.isEmpty()){
                     int l = pQ.poll();
                     arr2.add(l);
-                    tmp.add(l);
                 }
-                answer[0] = arr2.get(arr2.size()-1);
-                answer[1] = arr2.get(0);
+                answer[0] = arr2.get(arr2.size()-1);    // 최댓값
+                answer[1] = arr2.get(0);        // 최솟값
             }
 
             return answer;
