54687d860f751075320571827f84a7d1a75e07f6
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-07-20 16:44:47 +0900

    [Gold IV] Title: LCS 2, Time: 160 ms, Memory: 18968 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -6,19 +6,18 @@ import java.util.Arrays;
 
 public class Main {
     static String s1, s2;
-    static char[] c1,c2;
+    static char[] c1, c2;
     static int[][] dp;
     static int max;
     static char[] ans;
 
     static int Solution(int s1Len, int s2Len) {
-        for(int i =1; i<s1Len+1;i++) {
-            for(int j=1;j<s2Len+1;j++) {
-                if(c1[i] == c2[j]) dp[i][j] = dp[i-1][j-1] +1;
+        for (int i = 1; i < s1Len + 1; i++) {
+            for (int j = 1; j < s2Len + 1; j++) {
+                if (c1[i] == c2[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
                 else {
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
-
 
 
             }
@@ -28,24 +27,23 @@ public class Main {
         return dp[s1Len][s2Len];
     }
 
-    static void LCS(int i,int j,int k) {
-        if(i ==0 || j == 0) {
+    static void LCS(int i, int j, int k) {
+        if (i == 0 || j == 0) {
             return;
-        } else if(c1[i] == c2[j]) {
+        } else if (c1[i] == c2[j]) {             // 두 문자가 같을 시
             ans[k] = c1[i];
-            LCS(i-1,j-1,++k);
-        }
-        else {
-            if(dp[i][j] == dp[i-1][j])  LCS(i-1,j,k);
+            LCS(i - 1, j - 1, --k);
+        } else {      // 두 문자가 다를 시
+            if (dp[i][j] == dp[i - 1][j]) LCS(i - 1, j, k);
             else
-                LCS(i,j-1,k);
+                LCS(i, j - 1, k);
         }
 
 
     }
 
 
-    public static void main(String[] args ) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -57,28 +55,26 @@ public class Main {
         int s2Len = s2.length();
 
         // String -> 문자배열로 바꿔서 넣어줌.
-        c1 = new char[s1Len+1];
-        c2 = new char[s2Len+1];
+        c1 = new char[s1Len + 1];
+        c2 = new char[s2Len + 1];
+        for (int i = 1; i < s1Len + 1; i++) c1[i] = s1.charAt(i - 1);
+        for (int j = 1; j < s2Len + 1; j++) c2[j] = s2.charAt(j - 1);
 
-        for(int i=1;i<s1Len+1;i++) c1[i] = s1.charAt(i-1);
-        for(int j=1;j<s2Len+1;j++) c2[j] = s2.charAt(j-1);
+        dp = new int[s1Len + 1][s2Len + 1];
 
-        dp = new int[s1Len+1][s2Len+1];
+        // LCS 의 길이 max 에 넣어줌.
+        max = Solution(s1Len, s2Len);
+        bw.write(max + "\n");
 
-        max = Solution(s1Len,s2Len);
-        bw.write(max+"\n");
 
-        if(max == 0) bw.write("");
+        if (max == 0) bw.write("");
         else {
             ans = new char[max];
-            LCS(s1Len,s2Len,0);
-            for(int x = max-1;x>=0;x--)
+            LCS(s1Len, s2Len, max - 1);
+            for (int x = 0; x < max; x++)
                 bw.write(ans[x]);
 
         }
-
-
-
 
 
         bw.flush();
@@ -86,19 +82,7 @@ public class Main {
         bw.close();
 
 
-
-
-
-
-
-
-
-
-
-
     }
-
-
 
 
 }
