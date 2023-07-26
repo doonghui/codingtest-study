21eceb632776e290e653c27d0c9d7f71a205b374
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-07-26 16:10:01 +0900

    [Gold IV] Title: 뱀, Time: 180 ms, Memory: 16228 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -8,10 +8,11 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 
-class TurnCnt{
+class TurnCnt {
     int cnt;
     char turn;
-    public TurnCnt(int cnt,char turn) {
+
+    public TurnCnt(int cnt, char turn) {
         this.cnt = cnt;
         this.turn = turn;
     }
@@ -32,49 +33,52 @@ class Point {
 
 public class Main {
 
-    static int n,k,l;
+    static int n, k, l;
     static int[][] arr;
     static ArrayList<TurnCnt> list;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
     static int idx;
 
     static int bfs() {
         Queue<Point> q = new LinkedList<>();
         arr[1][1] = 1;
-        q.offer(new Point(1,1));
+        q.offer(new Point(1, 1));               // 최초 뱀의 위치
         int count = 0;
         idx = 0;
         int nx = 1;
         int ny = 1;
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             count++;
-             nx += dx[idx];
-             ny += dy[idx];
+            nx += dx[idx];
+            ny += dy[idx];
 
-            if(nx <1 || ny < 1 || nx > n || ny > n || arr[nx][ny] == 1) return count;
+            // 벽이나 자기 몸과 부딪혔을 때
+            if (nx < 1 || ny < 1 || nx > n || ny > n || arr[nx][ny] == 1) return count;
 
-            if(arr[nx][ny] == 2) {
+            // 이동한 곳에 사과가 있을 때
+            if (arr[nx][ny] == 2) {
                 arr[nx][ny] = 1;
-                q.add(new Point(nx,ny));
+                q.add(new Point(nx, ny));
 
             } else {
                 arr[nx][ny] = 1;
-                q.add(new Point(nx,ny));
-                Point po = q.poll();
+                q.add(new Point(nx, ny));
+                Point po = q.poll();            // 꼬리부분 삭제
                 arr[po.x][po.y] = 0;
             }
 
 
-            for(TurnCnt tc : list) {
-                if(tc.cnt == count) {
-                    if(tc.turn == 'D') {
-                        if(idx == 3) idx = 0;
+            // count 초에 방향을 전환시킬지 아닌지 확인
+            for (TurnCnt tc : list) {
+                if (tc.cnt == count) {
+                    if (tc.turn == 'D') {
+                        if (idx == 3) idx = 0;
                         else
                             idx++;
                     } else {
-                        if(idx == 0) idx = 3;
+                        if (idx == 0) idx = 3;
                         else
                             idx--;
                     }
@@ -95,29 +99,30 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
         k = Integer.parseInt(br.readLine());
-        arr = new int[n+1][n+1];
+        arr = new int[n + 1][n + 1];
 
-        for(int i = 0;i<k;i++) {
-            st = new StringTokenizer(br.readLine()," ");
+
+        // 사과 위치
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[b][a] = 2;
+            arr[a][b] = 2;
         }
 
+        // 방향 전환 정보
         l = Integer.parseInt(br.readLine());
-
-         list = new ArrayList<>();
-        for(int i = 0;i<l;i++) {
-            st = new StringTokenizer(br.readLine()," ");
+        list = new ArrayList<>();
+        for (int i = 0; i < l; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
             int t = Integer.parseInt(st.nextToken());
             char c = st.nextToken().charAt(0);
-            list.add(new TurnCnt(t,c));
+            list.add(new TurnCnt(t, c));
         }
-
 
 
         br.close();
-        bw.write(        bfs()+ "");
+        bw.write(bfs() + "");
         bw.flush();
         bw.close();
 
