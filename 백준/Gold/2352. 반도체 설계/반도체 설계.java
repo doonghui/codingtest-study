243aed9ba32574042cccc900bb5b3e0ba8ff0d6f
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-09-11 08:34:49 +0900

    Update 반도체 설계.java

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -2,15 +2,15 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
+
 
 class Main {
 
-    static int n, m;
+    static int n;
     static int[] arr;
-    static ArrayList<Integer> list;
+    static ArrayList<Integer> dist;
 
 
     public static void main(String[] args) throws Exception {
@@ -19,57 +19,58 @@ class Main {
         StringTokenizer st = null;
 
         n = Integer.parseInt(br.readLine());
-        list = new ArrayList<>();
 
-        arr = new int[n];
+        arr = new int[n+1];
+        dist = new ArrayList<>();
+
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        sol();
+        lis();
 
 
 
-        bw.write(list.size()-1 + "");
-        bw.flush();
         br.close();
+        bw.write(dist.size()-1 + "");
+        bw.flush();
         bw.close();
-    }
-
-
-    static void sol() {
-        list.add(-1_000_000_001);
-
-        for (int i = 0; i < n; i++) {
-            if(arr[i] > list.get(list.size()-1)) list.add(arr[i]);
-            else
-                binarySearch(i);
-        }
 
     }
 
+    static void lis() {
+        dist.add(0);            // 임의의 아무값
+        dist.add(arr[1]);
 
-
-
-    static void binarySearch(int idx) {
-        int left = 0;
-        int right = list.size()-1;
-        int mid;
-        while(left < right) {
-            mid = (left + right)/2;
-            if(list.get(mid) < arr[idx]) {
-                left = mid+1;
-            } else if(list.get(mid) >= arr[idx]) {
-                right = mid;
+        for (int i = 2; i <= n; i++) {
+            if (dist.get(dist.size()-1) < arr[i]) {
+                dist.add(arr[i]);
             }
+            else {
+                int idx = binarySearch(1,dist.size()-1, i);
+                dist.set(idx,arr[i]);
+
+            }
+
         }
 
-        list.set(right,arr[idx]);
 
     }
+
+    static int binarySearch(int left, int right, int idx) {
+        int mid = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (dist.get(mid) < arr[idx]) left = mid + 1;
+            else if (dist.get(mid) > arr[idx]) right = mid-1;
+            else return mid;
+
+        }
+
+        return left;
+    }
+
 
 }
-
-
 
