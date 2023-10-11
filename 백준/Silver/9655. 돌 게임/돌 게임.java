c86f5fa1f53860f4cde983a3e3b5b3992d94b2a3
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-10-11 23:36:45 +0900

    Update 돌 게임.java

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -22,11 +22,7 @@ public class Main {
 //        else
 //            ans = "CY";
 
-
         dp[1] = true;
-        dp[2] = false;
-        dp[3] = true;
-
         for (int i = 3; i <= n; i++) {
             dp[i] = !dp[i - 1];
         }
