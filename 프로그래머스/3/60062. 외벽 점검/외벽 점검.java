a521293a3b5e105c1941a72c68d1e56acb7ab76c
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-11-13 15:05:53 +0900

    [level 3] Title: 외벽 점검, Time: 62.04 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -1,41 +1,37 @@
 import java.util.*;
 class Solution {
-    static boolean flag = false;
     static int answer = Integer.MAX_VALUE;
-    static  int[] weak_append;
+    static  int[] newWeak;
 
    
+    
     public  int solution(int n, int[] weak, int[] dist) {
 
-//         Arrays.sort(dist,Collections.reverseOrder());       // 내림차순 정렬
-
-//        ArrayList<Integer> distance = new ArrayList<>();
 
         boolean[] visited = new boolean[dist.length];
         ArrayList<Integer> visitArr = new ArrayList<>();
 
-        weak_append = new int[weak.length*2];
-        int j=0;
-        while (j<weak.length){
-            weak_append[j] = weak[j];
-            weak_append[j + weak.length] = weak[j++]+n;
+        newWeak = new int[weak.length*2];
+
+        for(int j =0; j<weak.length;j++) {
+            newWeak[j] = weak[j];
+            newWeak[j + weak.length] = weak[j]+n;
         }
 
 
         for (int i = 0; i < weak.length; i++) {              // 취약지점 선택
-            select(i, 0, dist, visited, weak, visitArr, n);
+            select(i, 0, dist, visited,visitArr);
         }
         
-        if(answer == Integer.MAX_VALUE) answer = -1;
+        if(answer == Integer.MAX_VALUE) answer =-1;
 
         return answer;
     }
 
-    static void select(int weakIdx, int dept, int[] dist, boolean[] visited, int[] weak, ArrayList<Integer> visitArr, int n) {
+    static void select(int weakIdx, int dept, int[] dist, boolean[] visited,ArrayList<Integer> visitArr) {
         if (dept == dist.length) {
-            int ans = check(weakIdx, visitArr, weak, n);
+            int ans = check(weakIdx, visitArr);
             if (ans != Integer.MAX_VALUE) {
-
                 answer = Math.min(answer, ans);
                 return;
             }
@@ -47,7 +43,7 @@ class Solution {
             if (!visited[i]) {
                 visited[i] = true;
                 visitArr.add(dist[i]);
-                select(weakIdx, dept + 1, dist, visited, weak, visitArr, n);
+                select(weakIdx, dept + 1, dist, visited, visitArr);
                 visited[i] = false;
                 visitArr.remove(visitArr.size()-1);
 
@@ -57,26 +53,24 @@ class Solution {
 
     }
 
-    static int check(int weakIdx, ArrayList<Integer> visitArr, int[] weak, int n) {
-        int start = weakIdx;
-        int end = start+weak_append.length/2;
+    static int check(int weakIdx, ArrayList<Integer> visitArr) {
+        int start = weakIdx;                    // 시작 인덱스
+        int end = start+newWeak.length/2;       // 마지막 인덱스
 
 
-        int friend = 1;// permuted의 index
-        int pos = weak_append[start]+visitArr.get(friend-1);// 첫 취약점의 위치+친구거리 에서 시작
+        int people = 1;
+        int distance = newWeak[start]+visitArr.get(0);
 
         for(int i=start; i<end; i++) {
-            if(pos < weak_append[i]) {// 점검 위치 벗어나면
-                friend++; // 친구추가
-                if (friend > visitArr.size()) return Integer.MAX_VALUE; // 친구 초과하면 실패한 방법
-                pos = weak_append[i] + visitArr.get(friend-1);
+            if(distance < newWeak[i]) {
+                people++;
+                if (people > visitArr.size()) return Integer.MAX_VALUE;
+                distance = newWeak[i] + visitArr.get(people-1);
             }
         }
-        return friend;
+        return people;
 
     }
-
-
 }
 
 
