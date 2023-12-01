bd9d48134823ce045e7995c73bae316b03d839c0
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-12-01 13:48:51 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 10.72 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    static boolean flag = false;
+    public int[] solution(long[] numbers) {
+        int[] answer = new int[numbers.length];
+        int idx =0;
+        
+        for(long number : numbers) {
+        String binaryNumber = Long.toBinaryString(number);
+            
+        int nodNumber = 0;
+        int height = 1;
+        while(nodNumber < binaryNumber.length()) {
+            nodNumber = (int)Math.pow(2,height) -1;
+            height++;
+        }
+            
+        int[] binary = new int[nodNumber];      // 이진수 배열로 저장
+        int len = nodNumber-binaryNumber.length();
+            
+        for(int i = 0;i <len;i++) {
+           binary[i] = 0;
+        }
+
+        for(int j = len;j <nodNumber;j++) {
+           binary[j] = binaryNumber.charAt(j-len)-'0';
+        }
+            
+           
+        check(0,binary.length-1,binary);
+        if(flag) {
+            answer[idx] = 0;
+            flag = false;
+        } else {
+            answer[idx] = 1;
+        }
+        idx++;
+        }
+            
+        return answer;
+    }
+    
+    
+    static void check(int lt,int rt,int[] binary) {
+        if(flag) return;
+        
+        int mid = (lt + rt) / 2;
+        int left =(lt + mid) /2;
+        int right =(mid+1 + rt) /2;
+       
+            if(binary[mid] ==0) {       // 부모 노드가 없는데 자식이 있으면 안됨!
+                if(binary[left] == 1 || binary[right] == 1) {
+                      flag = true;
+                return; 
+                }
+             }
+        
+        if(lt == rt) return;
+            
+        check(lt,mid-1,binary);
+        check(mid+1,rt,binary);
+            
+    }
+}
