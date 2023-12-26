3794dcb3ff86550d81a2a7af7bc529cb5be264ad
Donghui Park <62371491+doonghui@users.noreply.github.com>
2023-12-26 20:03:46 +0900

    Update 매칭 점수.java

--- 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
@@ -25,8 +25,6 @@ class URL implements Comparable<URL>{
 }
 
 class Solution {
-   
-  
     static ArrayList<URL> url;
 
     // 1. 웹 페이지 정보 가져오기
@@ -58,22 +56,7 @@ class Solution {
             }
         }
 
-        for(URL u : url) {
-            System.out.println("==================");
-            System.out.println("idx값: "+u.idx);
-            System.out.println("url이름: "+u.urlName);
-            System.out.println("기본점수: "+u.wordCnt);
 
-            System.out.println("외부링크개수: "+u.outerUrl.size());
-//
-//            for(String s : u.outerUrl) {
-//                System.out.print(s+" ");
-//            }
-            System.out.println("외부점수: "+u.outerScore);
-            System.out.println("총 점수: "+u.totalScore);
-
-            System.out.println("\n==================");
-        }
      double max = 0;
         int answer = 0;
         for(URL u: url){
@@ -87,22 +70,11 @@ class Solution {
             }
         }
 
-
-
         return answer;
 
     }
 
     static void check(String word, String page) {
-
-
-
-//        int urlIdx = page.indexOf("content=\"https://");
-//         int urlLastIdx = page.indexOf("\"/>",urlIdx);
-//        String sub = page.substring(urlIdx+17, urlLastIdx);
-//        System.out.println(sub);
-//        url.get(url.size()-1).urlName = sub;
-
         int left = 0;
         int right = 0;
         int mid = 0;
@@ -116,14 +88,11 @@ class Solution {
         url.get(url.size()-1).urlName = subUrl;
 
 
-//        page = page.substring(urlLastIdx);
 
         int bodyUrlIdx = page.indexOf("<body>");
         int bodyUrlLastIdx = page.indexOf("</body>",bodyUrlIdx);
 
         String bodyPage = page.substring(bodyUrlIdx+6, bodyUrlLastIdx);
-
-//        System.out.println(bodyPage);
 
         checkWord(word,bodyPage);
 
@@ -131,8 +100,7 @@ class Solution {
     }
  
     static void checkWord(String word,String bodyPage) {
-
-
+        
         double count = 0;
         String subBodyPage = bodyPage;
         while(subBodyPage.contains(word)) {
@@ -143,19 +111,15 @@ class Solution {
             }
             subBodyPage = subBodyPage.substring(wordLastIdx);
         }
-
-
-
+        
 
         while(bodyPage.contains("<a href=\"")) {
-
             int linkUrlIdx = bodyPage.indexOf("<a href=\"");
             int linkUrlLastIdx = bodyPage.indexOf("\">",linkUrlIdx);
 
             String outerUrl = bodyPage.substring(linkUrlIdx+9,linkUrlLastIdx);
 
             url.get(url.size()-1).outerUrl.add(outerUrl);
-            System.out.println(outerUrl);
 
             bodyPage = bodyPage.substring(linkUrlLastIdx);
         }
@@ -163,7 +127,6 @@ class Solution {
 
         url.get(url.size()-1).wordCnt = count;
         url.get(url.size()-1).totalScore = count;
-
         url.get(url.size()-1).outerScore =  url.get(url.size()-1).wordCnt /  url.get(url.size()-1).outerUrl.size();
     }
 
