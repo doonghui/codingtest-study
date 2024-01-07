a3d29d11e411593ec9103baea854aec4a9689b2a
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-01-07 22:54:09 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 808 ms, Memory: 59684 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -8,23 +8,23 @@ import java.util.StringTokenizer;
 class Point {
     int x;
     int y;
-    public Point(int x,int y ){
+
+    public Point(int x, int y) {
         this.x = x;
-        this.y =y;
+        this.y = y;
     }
 }
-
 
 
 class Main {
 
 
-    static int N,M;
+    static int N, M;
     static int[][] map;
     static int[][] newMap;
     static boolean[][] visited;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,1,-1};
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
 
     public static void main(String[] args) throws IOException {
@@ -32,7 +32,7 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = null;
 
-        st = new StringTokenizer(br.readLine()," ");
+        st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
@@ -41,31 +41,31 @@ class Main {
         visited = new boolean[N][M];
         int idxX = 0;
         int idxY = 0;
-        for(int i=0;i<N;i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            for(int j=0;j<M;j++) {
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 2) {
+                if (map[i][j] == 2) {
                     idxX = i;
                     idxY = j;
                 }
-                if(map[i][j] == 0 && !visited[i][j]) {
+                if (map[i][j] == 0) {
                     visited[i][j] = true;
                 }
             }
         }
 
-       bfs(idxX,idxY);
+        bfs(idxX, idxY);
 
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<M;j++) {
-             if(!visited[i][j]) newMap[i][j] = -1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j]) newMap[i][j] = -1;
             }
         }
 
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<M;j++) {
-                bw.write(newMap[i][j]+" ");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                bw.write(newMap[i][j] + " ");
             }
             bw.write("\n");
         }
@@ -76,31 +76,27 @@ class Main {
         br.close();
     }
 
-    static void bfs(int idxX,int idxY) {
+    static void bfs(int idxX, int idxY) {
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(idxX,idxY));
+        q.add(new Point(idxX, idxY));
         visited[idxX][idxY] = true;
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Point p = q.poll();
-            for(int i =0; i<4;i++) {
-                int nx = p.x+dx[i];
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
 
-                if(nx < 0 || nx >= N || ny <0 || ny >= M) continue;
-                if(map[nx][ny] == 1 && !visited[nx][ny]) {
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    q.add(new Point(nx,ny));
-                    newMap[nx][ny] = newMap[p.x][p.y] +1;
+                    q.add(new Point(nx, ny));
+                    newMap[nx][ny] = newMap[p.x][p.y] + 1;
                 }
 
             }
 
         }
-
-
-
-
 
 
     }
