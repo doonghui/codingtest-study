f1c938a80b4f896d2cd063b27c39321211262c69
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-01-13 04:17:16 +0900

    [Silver I] Title: 지름길, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -15,7 +15,6 @@ class Point implements Comparable<Point> {
 
     @Override
     public int compareTo(Point o) {
-        if (this.s == o.s) return this.e - o.e;
         return this.s - o.s;
     }
 
@@ -48,50 +47,23 @@ class Main {
             arr.add(new Point(s, e, len));
         }
 
-        for(int k = 0;k<=D;k++) dp[k] = k;
+        for (int k = 0; k <= D; k++) dp[k] = k;
 
         Collections.sort(arr);
-        int move = 0;
-        int idx = 0;
-        while(move < D) {
-            while(idx < arr.size()&& arr.get(idx).s == move) {
+        int move = 0;       // 고속도로 길이
+        int idx = 0;        // 지름길 idx
+        while (move < D) {
+            while (idx < arr.size() && arr.get(idx).s == move) {
                 Point p = arr.get(idx);
-//                if (dp[p.e] > dp[p.s] + p.len) {
-//                    dp[p.e] = dp[p.s] + p.len;
-//                }
-                dp[p.e] = Math.min(dp[p.e],dp[p.s] + p.len);
+                dp[p.e] = Math.min(dp[p.e], dp[p.s] + p.len);
 
                 idx++;
             }
 
-            dp[move+1] = Math.min(dp[move+1],dp[move] +1);
+            dp[move + 1] = Math.min(dp[move + 1], dp[move] + 1);
             move++;
         }
-//        for (int j = 0; j <D; j++) {
-//            if(p.s == j) {
-//                if (dp[p.e] > dp[p.s] + p.len) {
-//                    dp[p.e] = dp[p.s] + p.len;
-//                    if(j != 0) j = p.s -1;
-//                }
-//                idx++;
-//                if(idx == arr.size()) idx = 0;
-//                p = arr.get(idx);
-//
-//            }
-//                if (dp[j + 1] > dp[j] + 1) dp[j + 1] = dp[j] + 1;
-//
-//        }
 
-//        int idx = 0;
-//        while(!q.isEmpty()) {
-//            Point p = q.poll();
-//            if(dp[p.s] + p.len < dp[p.e]) {
-//                    idx = dp[p.s] + p.len;
-//                }
-//
-//            if(dp[idx +1] > dp[idx] +1) dp[idx+1] = dp[idx] +1;
-//
-//        }
         bw.write(dp[D] + "");
         br.close();
         bw.flush();
