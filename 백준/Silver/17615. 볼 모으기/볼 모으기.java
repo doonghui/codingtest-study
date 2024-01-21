9f30d997942230dc3629872b6948fa43382cfacf
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-01-21 17:50:36 +0900

    [Silver I] Title: 볼 모으기, Time: 124 ms, Memory: 14268 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        arr = new int[N];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') arr[i] = 0;
+            else
+                arr[i] = 1;
+        }
+
+        int answer = Math.min(sol(0),sol(1));
+
+
+
+        bw.write(answer+"");
+        br.close();
+        bw.flush();
+        br.close();
+    }
+
+    static int sol(int num) {
+        int count = 0;
+        boolean check = false;
+        for(int i = N-1;i>=0;i--) {
+            if(arr[i] == num) {
+              if(check) {
+                count++;
+              }
+            } else {
+                if(!check) check = true;
+            }
+        }
+
+
+        return count;
+    }
+
+
+}
