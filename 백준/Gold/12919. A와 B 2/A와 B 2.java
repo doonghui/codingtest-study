5007ff097a048f14d2ac32e636a9348af3438d38
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-01-30 18:32:09 +0900

    [Gold V] Title: A와 B 2, Time: 168 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -20,8 +20,8 @@ class Main {
         if (S.length() > T.length()) bw.write("0");
         else if (S.equals(T)) bw.write("1");
         else {
-            change(S.length(), T.length(),T);
-            if(flag) bw.write("1");
+            change(S.length(), T.length(), T);
+            if (flag) bw.write("1");
             else
                 bw.write("0");
 
@@ -34,8 +34,8 @@ class Main {
         br.close();
     }
 
-    static void change(int sLen,int tLen,String t) {
-        if(flag) return;
+    static void change(int sLen, int tLen, String t) {
+        if (flag) return;
         if (tLen == sLen) {
             if (t.equals(S)) {
                 flag = true;
@@ -46,19 +46,18 @@ class Main {
         }
 
 
-            if(t.charAt(t.length()-1) == 'A') {
-                change(sLen,tLen-1, t.substring(0,t.length()-1));
-            }
+        if (t.charAt(t.length() - 1) == 'A') {
+            change(sLen, tLen - 1, t.substring(0, t.length() - 1));
+        }
 
-            if(t.charAt(0) == 'B') {
-                t = t.substring(1);
-               String tmp = "";
-               for(int k = t.length()-1;k>=0;k--) {
-                   tmp += t.charAt(k);
-               }
-                t = tmp;
-                change(sLen,tLen-1,t);
+        if (t.charAt(0) == 'B') {
+            t = t.substring(1);
+            String tmp = "";
+            for (int k = t.length() - 1; k >= 0; k--) {
+                tmp += t.charAt(k);
             }
+            change(sLen, tLen - 1, tmp);
+        }
 
     }
 
