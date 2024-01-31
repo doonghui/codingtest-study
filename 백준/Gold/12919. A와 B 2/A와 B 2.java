4e1dbc16b70a7abe0146254ddaa61e4ae1890016
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-02-01 00:30:28 +0900

    [Gold V] Title: A와 B 2, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,65 +1,44 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Main {
+public class Main {
 
-    static String S, T;
-    static boolean flag = false;
+    static int answer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = null;
-
-        S = br.readLine();
-        T = br.readLine();
-
-        if (S.length() > T.length()) bw.write("0");
-        else if (S.equals(T)) bw.write("1");
-        else {
-            change(S.length(), T.length(), T);
-            if (flag) bw.write("1");
-            else
-                bw.write("0");
-
-
-        }
-
-
-        br.close();
-        bw.flush();
-        br.close();
-    }
-
-    static void change(int sLen, int tLen, String t) {
-        if (flag) return;
-        if (tLen == sLen) {
-            if (t.equals(S)) {
-                flag = true;
-                return;
-            }
-
+    private static void dfs(String S,String T){
+        StringBuffer sb = new StringBuffer(T);
+        if(S.length() == T.length()) {
+              if (T.equals(S)){
+            answer = 1;
             return;
         }
-
-
-        if (t.charAt(t.length() - 1) == 'A') {
-            change(sLen, tLen - 1, t.substring(0, t.length() - 1));
+            return;
         }
-
-        if (t.charAt(0) == 'B') {
-            t = t.substring(1);
-            String tmp = "";
-            for (int k = t.length() - 1; k >= 0; k--) {
-                tmp += t.charAt(k);
-            }
-            change(sLen, tLen - 1, tmp);
+      
+      
+        if (T.endsWith("A")){
+            T = T.substring(0, T.length()-1);
+            dfs(S,T);
+        }
+       if (T.startsWith("B")){
+            T = sb.reverse().toString();
+            T = T.substring(0, T.length()-1);
+            dfs(S,T);
         }
 
     }
 
 
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        String T = br.readLine();
+
+        answer = 0;
+
+        dfs(S,T);
+
+        System.out.println(answer);
+
+    }
 }
