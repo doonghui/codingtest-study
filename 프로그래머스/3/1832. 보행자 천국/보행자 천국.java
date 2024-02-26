e0d3d2f82fc1afc59558242596020e2dc2b66b08
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-02-26 18:05:00 +0900

    [level 3] Title: 보행자 천국, Time: 171.32 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,53 +1,54 @@
 import java.util.*;
 
 class Solution {
-    
+    static int MOD = 20170805;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,-1,0,1};
+    static int[][][] dp;
     public int solution(int m, int n, int[][] cityMap) {
+        int answer = 0;
+       
+        dp = new int[m][n][2];      // 위 -> 아래 : 0 , 왼 -> 오 : 1
         
-       long[][][] dp = new long[m][n][2];
-        int MOD = 20170805;
-
-        sol(m,n,cityMap,dp,MOD);
-        
-        int answer = (int)((dp[m - 1][n - 1][0] + dp[m-1][n-1][1])%MOD);
-        
-        
-        
-        return answer;
+        dp(m,n,cityMap);
+           
+        return (dp[m-1][n-1][0] + dp[m-1][n-1][1]) %MOD;
     }
     
     
-        static void sol(int m, int n, int[][] cityMap,long[][][] dp,int MOD) {
-        dp[0][0][0] = 1;
-        dp[0][0][1] = 1;
+      static void dp(int m, int n, int[][] cityMap) {
 
-        for(int i= 0; i<m;i++){
-            if(cityMap[i][0] == 1) break;
+        for (int i = 0; i < m; i++) {
+            if (cityMap[i][0] == 1) break;
             dp[i][0][0] = 1;
         }
-        for(int j= 0; j<n;j++){
-            if(cityMap[0][j] == 1) break;
+        for (int j = 0; j < n; j++) {
+            if (cityMap[0][j] == 1) break;
             dp[0][j][1] = 1;
         }
 
-        // 1. 전 위치가 모든 0 일때
-        // 2. 전 위치중 하나라도 2일때
         for (int x = 1; x < m; x++) {
             for (int y = 1; y < n; y++) {
-                if(cityMap[x][y-1] != 1) {          // 위쪽
-                    if(cityMap[x][y-1] == 0)  dp[x][y][1] = (dp[x][y-1][0] + dp[x][y-1][1]) % MOD;
-                    else
-                        dp[x][y][1] = dp[x][y-1][1] % MOD;
-                }
-                if(cityMap[x-1][y] != 1) {          // 왼쪽
-                    if(cityMap[x-1][y] == 0)  dp[x][y][0] = (dp[x-1][y][0] + dp[x-1][y][1]) % MOD;
-                    else dp[x][y][0] = dp[x-1][y][0] % MOD;
+                if (cityMap[x - 1][y] != 1) {          // 위에서 아래
+                    if (cityMap[x - 1][y] == 0) {
+                        dp[x][y][0] = (dp[x - 1][y][0] + dp[x - 1][y][1]) % MOD;
+                    } else {
+                        dp[x][y][0] = dp[x - 1][y][0];
+                    }
                 }
 
+                if (cityMap[x][y - 1] != 1) {        // 왼쪽에서 오른쪽
+                    if (cityMap[x][y - 1] == 0) {
+                        dp[x][y][1] = (dp[x][y - 1][0] + dp[x][y - 1][1]) % MOD;
+                    } else {
+                        dp[x][y][1] = dp[x][y - 1][1];
+                    }
+                }
 
             }
         }
-
-
+        
     }
+    
+    
 }
