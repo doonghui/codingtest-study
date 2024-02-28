32d7fe081c5ba649a774725ffce659bdb00c1bfc
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-02-28 16:08:04 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.06 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,68 +1,60 @@
 import java.util.*;
+
 class Solution {
-    static int[] dx = {1,0,0,-1};         // 아래,왼,오,위
-    static int[] dy = {0,-1,1,0};  
+
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, -1, 1, 0};
+    static int[][] map;
     static char[] arr;
-    static String ans = "";
-    
+    static boolean flag = false;
+
+
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         String answer = "";
-        
-        int distance = Math.abs(x - r) + Math.abs(y - c);
-
-        if(distance > k || (k - distance) % 2 == 1) return "impossible";        // 거리가 k 보다 크거나 중복방문시 홀수면 x
-        
+        map = new int[n + 1][m + 1];
+        int sub = Math.abs(x - r) + Math.abs(y - c);
         arr = new char[k];
-        dfs(0,n,m,x,y,r,c,k);
-        
-        if(ans == "") answer = "impossible";
-        else
-            return ans;
-        
+        if (sub > k || Math.abs(sub - k) % 2 == 1) answer = "impossible";           // 테스트3 틀린이유
+        else {
+            sol(0, n, m, x, y, r, c, k);
+            answer = new String(arr);
+        }
+
+
         return answer;
     }
-    
-    
-    static void dfs(int dept,int n, int m,int x, int y, int r, int c, int k) {
-        
-        if(k < dept + Math.abs(x - r) + Math.abs(y - c)) return;
-        
-        if(dept == k && x == r && y == c) {
-            String tmp = new String(arr);
-            // if(ans.equals("")) ans = tmp;
-            // else {
-            //     if(tmp.compareTo(ans) <=0) ans = tmp;
-            // }
-            ans = tmp;
-            
+
+    static void sol(int dept, int n, int m, int x, int y, int r, int c, int k) {
+        if (k < dept + Math.abs(x - r) + Math.abs(y - c)) return;                 // 시간초과나 런타임 이유
+
+        if (dept == k && x == r && y == c) {
+            flag = true;
             return;
         }
-        if(dept == k) return;
-        
-        for(int i =0 ;i<4;i++) {
+
+        if (dept == k) {
+            return;
+        };
+
+
+        for (int i = 0; i < 4; i++) {
+            if (flag) return;
             int nx = x + dx[i];
-            int ny = y + dy[i];        
-            if(nx < 1 || nx >n || ny < 1 || ny > m) continue;
-            
-            char tmpChar='0';
-             if(dx[i] == 1) tmpChar = 'd';
-            else if(dy[i] == -1) tmpChar = 'l';
-            else if(dy[i] == 1) tmpChar = 'r';
-            else if(dx[i] == -1) tmpChar = 'u';
-            if(!ans.equals("")) {
-                if(tmpChar >= arr[dept]) return;
-                else
-                    arr[dept] = tmpChar;
-                
-            } else{
-                arr[dept] = tmpChar;
-            }
-            
-            dfs(dept+1,n,m,nx,ny,r,c,k);
-            
-            
+            int ny = y + dy[i];
+
+            if (nx > n || nx < 1 || ny > m || ny < 1) continue;
+
+            char tmp = '0';
+            if (i == 0) tmp = 'd';
+            else if (i == 1) tmp = 'l';
+            else if (i == 2) tmp = 'r';
+            else if (i == 3) tmp = 'u';
+
+            arr[dept] = tmp;
+
+            sol(dept + 1, n, m, nx, ny, r, c, k);
         }
-        
-        
+
+
     }
 }
