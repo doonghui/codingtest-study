f3c35528bdc90b7f8f8598349f34d6fff0ecba6d
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-07 20:49:45 +0900

    [level 3] Title: 110 옮기기, Time: 63.82 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+// 5, 9~ 17 빼고 나머지 시간초과.. 어케 줄일까
+class Solution {
+    public String[] solution(String[] s) {
+      String[] answer = new String[s.length];
+        ArrayList<String> arr = new ArrayList<>();
+
+        // s에서 110을 다 제외해준다.
+        // 남은 s 중 뒤에서부터 확인해보고 0이 나오는 순간 그 뒤에 110 을 다 붙여준다.
+        // 만약 끝까지 했을 때 0이 없으면 맨 앞에 110을 다 붙여준다.
+        for (String str : s) {
+            if (str.length() <= 3) {
+                arr.add(str);
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbA = new StringBuilder();
+
+            for (int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == '0') {
+                    int len = sb.length();
+                    if(len >= 2 && sb.charAt(len-2) == '1' && sb.charAt(len-1) == '1') {
+                        sb.delete(len-2,len);
+                        sbA.append("110");
+                    } else
+                        sb.append(str.charAt(i));
+                } else {
+                    sb.append(str.charAt(i));
+                }
+
+            }
+
+
+//            while (true) {
+//                if (sb.length() <= 3 || idx > sb.length() - 3) break;
+//                String st = sb.substring(idx, idx + 3);
+//                if (st.equals("110")) {
+//                    sb.delete(idx, idx + 3);
+//                    sbA.append("110");
+//                    idx = 0;
+//                } else
+//                    idx++;
+//            }
+//            boolean flag = false;
+
+            if (sb.indexOf("0") == -1) {
+                sb.insert(0, sbA.toString());
+            } else {
+                sb.insert(sb.lastIndexOf("0") + 1, sbA.toString());
+            }
+
+
+//            for (int i = sb.length() - 1; i >= 0; i--) {
+//                if (sb.charAt(i) == '0') {
+//                    flag = true;
+//                    sb.insert(i + 1, sbA.toString());
+//                    break;
+//                }
+//            }
+//
+//            if (!flag) {
+//                sb.insert(0, sbA.toString());
+//            }
+
+            arr.add(sb.toString());
+        }
+
+        for (int i = 0; i < s.length; i++) {
+            answer[i] = arr.get(i);
+        }
+
+
+        return answer;
+    }
+
+    
+
+}
