ec492226f8491cad924171c8b488397bf3ac86c7
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-13 18:11:23 +0900

    [level 3] Title: 스타 수열, Time: 0.58 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Point implements Comparable<Point> {
+    int key;
+    int value;
+    
+    public Point(int key, int value) {
+        this.key =key;
+        this.value = value;
+    }
+    @Override
+    public int compareTo(Point o) {
+        return o.value - this.value;        
+    }
+}
+
+
+// 42.9 -> 46.4
+class Solution {
+    public int solution(int[] a) {
+        int answer = -1;
+        
+        ArrayList<Point> arr = new ArrayList<>();
+        for(int i =0;i<a.length;i++) arr.add(new Point(i,0));
+        
+        if(a.length <2) answer = 0;
+        else {
+         for(int i = 0;i<a.length;i++) {
+             arr.get(a[i]).value += 1;
+         }
+        
+        Collections.sort(arr);
+
+            
+        for(Point p : arr) {
+            if(p.value < answer) continue;           // 테스트13 부터 시간초과인 이유 .. 이거해도 14,15 초과함 -> 처음에 큰 것부터 반복
+            int cnt = 0;
+            for(int j =0;j<a.length-1;j++) {
+                if(a[j] != a[j+1] && (a[j] == p.key || a[j+1] == p.key)) {
+                    cnt++;
+                    j++;
+                }
+ 
+            }
+            
+            answer = Math.max(answer,cnt);
+        }
+        }
+        
+        return answer *= 2;
+    }
+}
