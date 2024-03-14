4eb09dcd7001da4cd439ce2ddcc5d7d97033d395
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-14 15:47:58 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 24.97 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,66 +1,71 @@
 import java.util.*;
 
+// 42 -> 101010
 class Solution {
-    static boolean flag = false;
+    
+    static boolean flag;
     public int[] solution(long[] numbers) {
         int[] answer = new int[numbers.length];
-        int idx =0;
+        int idx = 0;
         
         for(long number : numbers) {
-        String binaryNumber = Long.toBinaryString(number);
+            String str = Long.toBinaryString(number); 
             
-        int nodNumber = 0;
-        int height = 1;
-        while(nodNumber < binaryNumber.length()) {
-            nodNumber = (int)Math.pow(2,height) -1;
-            height++;
-        }
+            int h = 1;      // 이진트리의 높이
+            while(Math.pow(2,h)-1 < str.length()) {
+               h++;
+            }
             
-        int[] binary = new int[nodNumber];      // 이진수 배열로 저장
-        int len = nodNumber-binaryNumber.length();
+            if(Math.pow(2,h)-1 != str.length()) {       // 포화이진트리 만들기
+                while(Math.pow(2,h)-1 != str.length()) {
+                    str = "0" + str;
+                }
+            }
+            char[] binary = str.toCharArray(); 
             
-        for(int i = 0;i <len;i++) {
-           binary[i] = 0;
-        }
-
-        for(int j = len;j <nodNumber;j++) {
-           binary[j] = binaryNumber.charAt(j-len)-'0';
-        }
-            
-           
-        check(0,binary.length-1,binary);
-        if(flag) {
-            answer[idx] = 0;
             flag = false;
-        } else {
-            answer[idx] = 1;
-        }
-        idx++;
-        }
+            int mid = binary.length /2;
+            int lt = mid /2;
+            int rt = mid + (binary.length - mid) /2;
+            check(0,binary.length-1,binary);
             
+            if(!flag) {
+                answer[idx] = 1;
+            } else
+                answer[idx] = 0;
+            
+           idx++; 
+        }
+        
+        
+        
+        
         return answer;
     }
     
     
-    static void check(int lt,int rt,int[] binary) {
+    static void check(int lt,int rt,char[] binary) {
         if(flag) return;
-        if(binary.length == 1) {
-            if(binary[0] == 0) flag = true;
-        }
+        
+//         if(binary.length == 1) {
+//             if(binary[0] == 0) flag = true;
+//         }
+        
         if(lt == rt) return;
-        int mid = (lt + rt) / 2;
-        int left =(lt + mid-1) /2;
-        int right =(mid+1 + rt) /2;
-       
-            if(binary[mid] ==0) {       // 부모 노드가 없는데 자식이 있으면 안됨!
-                if(binary[left] == 1 || binary[right] == 1) {
-                      flag = true;
-                return; 
-                }
-             }
-            
+        int mid = (lt + rt) /2;
+        int left = (mid-1 + lt) /2;
+        int right = (mid+1 + rt) /2;
+        
+        
+        if(binary[mid] == '0') {
+            if(binary[left] == '1' || binary[right] == '1') {
+                flag = true;
+                return;
+            }
+        }
+        
         check(lt,mid-1,binary);
         check(mid+1,rt,binary);
-            
     }
 }
+
