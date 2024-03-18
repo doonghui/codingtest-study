55783e40c36df144b9faeb1fca8ea9d69c32db43
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-18 22:35:21 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -17,31 +17,24 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         int[] dp = new int[1000001];
-        if (N == 1) {
-            bw.write("0");
-        } else if (N <= 3) {
-            bw.write("1");
-        } else {
-            dp[2] = 1;
-            dp[3] = 1;
-            for (int i = 4; i <= N; i++) {
-                if (i % 2 == 0 && i % 3 == 0) {
-                    dp[i] = Math.min(dp[i / 3], (Math.min(dp[i / 2], dp[i - 1]))) + 1;
-                } else if (i % 2 == 0) {
-                    dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
-                } else if (i % 3 == 0) {
-                    dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
-                } else if (i % 2 != 0 && i % 3 != 0) {
-                    dp[i] = dp[i - 1] + 1;
 
-                }
+        dp[2] = 1;
+        dp[3] = 1;
+        for (int i = 4; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3], (Math.min(dp[i / 2], dp[i - 1]))) + 1;
+            } else if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
+            } else if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
+            } else if (i % 2 != 0 && i % 3 != 0) {
+                dp[i] = dp[i - 1] + 1;
 
             }
-            bw.write(dp[N] + "");
 
         }
 
-
+        bw.write(dp[N] + "");
         br.close();
         bw.flush();
         bw.close();
