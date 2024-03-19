6a8cba6845f13858a3f2740ae7c25965ee5904f6
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-19 17:12:25 +0900

    [Silver III] Title: Four Squares, Time: 164 ms, Memory: 15388 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,50 +1,37 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 
 public class Main {
 
-    static int[] dp;
-    static int n, m;
-    static int cnt = 4;
-    static int min;
+    static int N;
 
-    static void Sol(int i) {
-
-
-        for(int j =1;j*j <= i;j++) {
-            int tmp = i - j*j;
-            min = Math.min(dp[tmp],min);
-        }
-
-    }
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = null;
 
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        dp = new int[n+1];
+        int[] dp = new int[N + 1];
         dp[1] = 1;
-        for(int i=2; i<=n;i++) {
-            min = Integer.MAX_VALUE;
-            Sol(i);
-            dp[i] = min +1;
+
+        for (int i = 2; i <= N; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                int num = i - j * j;
+                min = Math.min(dp[num], min);
+            }
+            dp[i] = min + 1;
         }
 
-
-        bw.write(dp[n]+" ");
+        bw.write(dp[N] + "");
         br.close();
         bw.flush();
         bw.close();
     }
+
+
 }
-
-
