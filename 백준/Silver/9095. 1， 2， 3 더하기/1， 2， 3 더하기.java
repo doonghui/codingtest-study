6ad4bbb6cad3c55bc59b6633fdaf7b0dbc7ecc04
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-19 17:03:17 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 136 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,41 +1,42 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dp;
-    static int[] ch;
-
-    static int Sol(int n) {
-        if(n == 1) return dp[n] = 1;
-        if(n == 2) return dp[n] = 2;
-        if(n == 3) return dp[n] = 4;
-
-        return dp[n] = Sol(n-1) + Sol(n-2) + Sol(n-3);
-    }
+    static int T,N;
 
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        T = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        dp = new int[11];
-        dp[0] = 0;
-
-        for (int i = 0; i < n; i++) {
-            int k = Integer.parseInt(br.readLine());
-            bw.write(Sol(k)+"\n");
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i =4;i<=10;i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
 
-        br.close();
 
+        for(int j =0;j<T;j++) {
+            N = Integer.parseInt(br.readLine());
+            bw.write(dp[N]+"\n");
+        }
+
+
+        br.close();
         bw.flush();
         bw.close();
     }
+
+
 }
