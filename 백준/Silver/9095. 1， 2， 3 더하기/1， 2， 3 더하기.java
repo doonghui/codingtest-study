a2e5a6a4f95081fcd8883f37fa2f5b798e4b43cc
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-19 17:04:06 +0900

    Update 1， 2， 3 더하기.java

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -18,7 +18,7 @@ public class Main {
 
         T = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[12];
+        int[] dp = new int[11];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
