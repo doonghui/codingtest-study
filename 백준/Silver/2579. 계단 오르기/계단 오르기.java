966d661e9b97120e4e98a54104f81b1a2aee54d8
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-20 16:35:56 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,54 +1,35 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
 
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
 
         N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N+1];
-        int[] dp = new int[N+1];
-
-        for(int i=1;i<=N;i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        
-        if(N == 1) {
-            bw.write(arr[1]+"");
-        } else if(N == 2) {
-            bw.write(arr[1]+arr[2]+"");
-        } else {
-            dp[0] = 0;
-            dp[1] = arr[1];
-            dp[2] = arr[1]+arr[2];
-
-            for(int j = 3;j<=N;j++) {
-                dp[j] = Math.max(arr[j]+dp[j-2],arr[j]+arr[j-1]+dp[j-3]);
+        int[] arr = new int[N + 1];
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(br.readLine());
+        dp[1] = arr[1];
+        if (N > 1) {
+            dp[2] = arr[1] + arr[2];
+            for (int i = 3; i <= N; i++) {
+                dp[i] = Math.max(arr[i - 1] + dp[i - 3], dp[i - 2]) + arr[i];
             }
-            bw.write(dp[N] + ""); 
-
         }
-     
 
-
-
-
+        bw.write(dp[N] + "");
         br.close();
         bw.flush();
         bw.close();
     }
 
 
-
 }
-
