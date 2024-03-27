6448edc5b1e58f1c9de031971e7040e4a0c198e2
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-27 18:07:34 +0900

    Update 가장 긴 증가하는 부분 수열.java

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,53 +1,44 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 import java.util.*;
 
 
 public class Main {
-    static int n;
-    static int[] arr;
-    static int[] dp;
+
+    static int N;
 
 
-    public static void Sol() {
-        dp[0] = 1;
-        for (int i = 1; i < n; i++) {
-            dp[i] = 1;
-            for (int j = i - 1; j >= 0; j--) {
-                if(arr[i] > arr[j] && dp[j] >= dp[i]) {
-                    dp[i] = dp[j] + 1;
-                }
-
-            }
-        }
-    }
-
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = null;
+        StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new int[n];
+        N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
         }
 
-        Sol();
         Arrays.sort(dp);
 
 
-
-
+        bw.write(dp[N - 1] + "");
         br.close();
-        bw.write(dp[n-1] + "");
         bw.flush();
         bw.close();
-
-
     }
+
+
 }
 
