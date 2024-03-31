033fdb33d9f7f9ce866d1f6c73f32f052d742095
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-31 17:13:00 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 284 ms, Memory: 21404 KB -BaekjoonHub

---
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class Main {
+
+    static int N, K;
+    static int[] dp;
+    static int[] arr;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        int lt = 0;
+        int count = 0;
+        int delete = 0;
+        if (arr[lt] % 2 == 0) {
+            count = 1;
+        } else {
+            delete++;
+        }
+        int max = count;
+        int rt = 1;
+        while (true) {
+            if (rt == N) break;
+            if (arr[rt] % 2 == 0) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                delete += 1;
+                while (delete > K) {
+                    if (arr[lt] % 2 == 0) count--;
+                    else
+                        delete--;
+                    lt++;
+                }
+            }
+            rt++;
+        }
+
+
+        bw.write(max + "");
+        bw.flush();
+        bw.close();
+    }
+
+}
+
