2accfd39504f4ede773d2b67961831c1fde1fd2a
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-03-31 17:16:11 +0900

    Update 가장 긴 짝수 연속한 부분 수열 （small）.java

--- 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
@@ -2,14 +2,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 import java.util.*;
 
 
 public class Main {
 
     static int N, K;
-    static int[] dp;
     static int[] arr;
 
 
@@ -35,6 +33,7 @@ public class Main {
         int max = 0;
         int lt = 0;
         int rt = 0;
+
         while (true) {
             if (rt == N) break;
             if (arr[rt] % 2 == 0) {
