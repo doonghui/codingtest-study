6dac52adea83bc152007017050a425cf4595c97a
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-04-02 17:41:58 +0900

    [Silver I] Title: 점프, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -19,7 +19,7 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        dp = new long[N][N];
+        dp = new long[N][N];                // int로 하면 틀리는 듯
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -29,32 +29,24 @@ public class Main {
         }
 
         int zero = map[0][0];
-
-
         dp[zero][0] = 1;
         dp[0][zero] = 1;
-        long count = 0;
+
         for (int x = 0; x < N; x++) {
             for (int y = 0; y < N; y++) {
                 int n = map[x][y];
-                if(x == N-1 && y == N-1) continue;
-                if(dp[x][y] < 1) continue;
+                if (x == N - 1 && y == N - 1) continue;
+                if (dp[x][y] < 1) continue;
 
-                if (x + n < N) {
-
-                            dp[x + n][y] += dp[x][y];
-                }
-                if (y + n < N) {
-                            dp[x][y+n] += dp[x][y];
-
-                }
+                if (x + n < N) dp[x + n][y] += dp[x][y];
+                if (y + n < N) dp[x][y + n] += dp[x][y];
 
 
             }
 
         }
 
-        bw.write(dp[N-1][N-1] + "");
+        bw.write(dp[N - 1][N - 1] + "");
         bw.flush();
         bw.close();
     }
