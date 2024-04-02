2e269a6bed5f15560cb7f5fd2d2cf4b1882eac76
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-04-02 17:40:34 +0900

    [Silver I] Title: 점프, Time: 132 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static long[][] dp;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        dp = new long[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int zero = map[0][0];
+
+
+        dp[zero][0] = 1;
+        dp[0][zero] = 1;
+        long count = 0;
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++) {
+                int n = map[x][y];
+                if(x == N-1 && y == N-1) continue;
+                if(dp[x][y] < 1) continue;
+
+                if (x + n < N) {
+
+                            dp[x + n][y] += dp[x][y];
+                }
+                if (y + n < N) {
+                            dp[x][y+n] += dp[x][y];
+
+                }
+
+
+            }
+
+        }
+
+        bw.write(dp[N-1][N-1] + "");
+        bw.flush();
+        bw.close();
+    }
+
+
+}
+
+
