70d78813ce18844a539343f1b8833c6601167ff5
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-04-09 14:05:33 +0900

    Update 호텔.java

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -1,31 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
-class Main {
-    static int c, n;
-    static int[] values;
-    public static void main(String[] args) throws IOException {
+
+public class Main {
+
+    static int C, N;
+    static int[] dp;
+
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        c = Integer.parseInt(st.nextToken());
-        values = new int[c+101];
-        Arrays.fill(values, Integer.MAX_VALUE);
-        n = Integer.parseInt(st.nextToken());
-        values[0] = 0;
-        for(int i=0; i<n; i++){
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        dp = new int[C + 101];
+
+        Arrays.fill(dp, 100000000);
+        dp[0] = 0;
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int cost = Integer.parseInt(st.nextToken());
-            int reward = Integer.parseInt(st.nextToken());
-            for(int j=reward; j<c+101; j++){
-                int prev = values[j-reward];
-                if(prev!=Integer.MAX_VALUE) values[j] = Math.min(values[j], cost+prev);
+            int m = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            for (int j = p; j < C + 101; j++) {             // 최소 C명이니깐 최대 C+100명까지
+                dp[j] = Math.min(dp[j], dp[j - p] + m);
             }
+
         }
-        int result = Integer.MAX_VALUE;
-        for(int i=c; i<c+101;i++){
-            result = Math.min(result,values[i]);
+
+        int min = Integer.MAX_VALUE;
+        for (int person = C; person < C + 101; person++) {
+            min = Math.min(min, dp[person]);
         }
-        System.out.println(result);
-        br.close();
+
+
+        bw.write(min + "");
+        bw.flush();
+        bw.close();
     }
+
+
 }
+
+
