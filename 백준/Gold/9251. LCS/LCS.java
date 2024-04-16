9ed7e6566b3c8d340691d3cc2d22a03b302102cd
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-04-16 21:24:35 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Main {
     static String s1, s2;
