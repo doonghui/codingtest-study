24b093ec4dc4b3c0c2557e50cecd91bd5c24dd4f
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-04-16 20:28:48 +0900

    [level 2] Title: [1차] 캐시, Time: 3.64 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 2.53 ms
+메모리: 73.2 MB, 시간: 3.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 30일 16:37:53
+2024년 04월 16일 20:28:43
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,29 +1,59 @@
 import java.util.*;
 
-
+// 11,16,19,20 실패
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-
-        LinkedList<String> cache = new LinkedList<>();
-   
-        for(int j = 0;j <cities.length;j++) {
-             if(cache.contains(cities[j].toLowerCase())) {
-                answer +=1;
-                 cache.remove(cities[j].toLowerCase());
-                 cache.add(cities[j].toLowerCase());
-            } else {
-                cache.add(cities[j].toLowerCase());
-                if(cache.size() > cacheSize) {
-                    cache.remove(0);
-                }
-                answer +=5;
+        
+        // LRU 알고리즘 : 가장 오랫동안 참조되지 않은 페이지를 교체하는 기법
+        
+        ArrayList<String> cache = new ArrayList<>();
+        for(int i=0;i<cities.length;i++) cities[i] = cities[i].toLowerCase();
+        
+        if(cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
+        boolean flag;
+        
+        int idx = 0;
+        // 원인 : 처음 캐시에 넣고나서 같은 도시가 또 나타날 때
+        while(true) {
+            if(cache.size() == cacheSize || idx >= cities.length) break;
+            flag = false;
+           for(int n = 0;n<cache.size();n++) {
+               if(cities[idx].equals(cache.get(n))) {
+                   cache.remove(cache.get(n));
+                   answer++;
+                   flag = true;
+                   break;
+               }
+           }
+            if(!flag) {
+                answer += 5;
             }
-            
-            
+            cache.add(cities[idx]);
+            idx++;
         }
         
         
+        for(int j = idx;j<cities.length;j++) {
+            String city = cities[j];
+            flag = false;
+            for(int k =0;k<cache.size();k++) {
+                if(city.equals(cache.get(k))) {
+                    cache.remove(cache.get(k));
+                    answer++;
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) {
+                cache.remove(0);
+                answer += 5;
+            }
+            cache.add(city);
+        }
         
         
         
