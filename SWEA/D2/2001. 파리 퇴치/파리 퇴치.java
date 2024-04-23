af6b17afb2cdb238e5fef2678de9c739063f2079
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-04-23 17:47:23 +0900

    [D2] Title: 파리 퇴치, Time: 116 ms, Memory: 21,572 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -3,62 +3,72 @@ import java.io.*;
 class Solution
 {
     static int T,N,M;
-	public static void main(String args[]) throws Exception
-	{
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[][] dp;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         T = Integer.parseInt(br.readLine());
-        
+
         for(int idx = 1; idx<=T;idx++) {
-        
+
             st = new StringTokenizer(br.readLine()," ");
-            
+
             N = Integer.parseInt (st.nextToken());
             M = Integer.parseInt (st.nextToken());
             int[][] map = new int[N][N];
+            dp = new int[N][N];
             for(int i =0;i<N;i++) {
-            st = new StringTokenizer(br.readLine()," ");
-            for(int j =0;j<N;j++) {
-            map[i][j] = Integer.parseInt(st.nextToken());
-            
+                st = new StringTokenizer(br.readLine()," ");
+                for(int j =0;j<N;j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    dp[i][j] = map[i][j];
+
+                }
             }
+
+            for(int i =0;i<N;i++) {
+                for(int j =0;j<N;j++) {
+                    if(i == 0 && j ==0) continue;
+                    else if(i == 0) dp[i][j] += dp[i][j-1];
+                    else if(j == 0) dp[i][j] += dp[i-1][j];
+                    else dp[i][j] += dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+                }
             }
-            
+
+
+
             int answer = solve(map);
-        
-        bw.write("#"+idx+" "+answer+"\n");
-        
+
+            bw.write("#"+idx+" "+answer+"\n");
+
         }
-        
+
         bw.flush();
         br.close();
         bw.close();
-       
-        
-        
-	}
-    static int solve(int[][] map) {
-    		
-        int max = 0;
-        for(int i =0;i<N;i++) {
-            for(int j =0;j<N;j++) {
-                int cnt =0;
-      	if(i+M > N || j+M > N) break;
-        for(int x = i;x<i+M;x++) {
-        		for(int y = j; y<j+M;y++) {
-                cnt += map[x][y];
-                
-                }
-        }
-                        max = Math.max(max,cnt);
-            }
-    
-        }
-    
-    
-    return max;
+
+
+
     }
-    
+    static int solve(int[][] map) {
+        int max = 0;
+
+        for(int x = M-1;x<N;x++) {
+            int count =0;
+            for(int y = M-1;y<N;y++) {
+                if(x -M <0 && y -M <0) count = dp[x][y];
+                else if(x-M < 0) count = dp[x][y] - dp[x][y-M];
+                else if(y-M < 0) count = dp[x][y] - dp[x-M][y];
+                else count = dp[x][y] -dp[x-M][y] - dp[x][y-M] + dp[x-M][y-M];
+                max = Math.max(max,count);
+            }
+
+        }
+
+        return max;
+    }
+
 }
