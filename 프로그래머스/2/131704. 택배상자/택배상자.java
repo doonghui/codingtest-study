f0fb3b40ccb1d2f77383ceb08a957b5668e7a52b
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-05-21 15:22:32 +0900

    [level 2] Title: 택배상자, Time: 128.66 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+class Solution {
+    public   int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack();
+
+        int idx = 0;
+      for(int n = 1;n<=order.length;n++) {
+          if(n == order[idx]) {
+              answer++;
+              idx++;
+          } else {
+              stack.push(n);
+          }
+          
+          while(!stack.isEmpty() && stack.peek() == order[idx]) {
+              stack.pop();
+              answer++;
+              idx++;
+          }
+          
+          
+      }
+
+        return answer;
+    }
+}
