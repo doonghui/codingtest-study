d32521b4b2189deef540ce867f93c59484863ca6
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-06-10 17:23:36 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 26.12 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+class Point {
+    int x;
+    int y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y =y;
+        
+    }
+}
+
+
+class Solution {
+    static Map<Integer,Integer> map;
+    static int cnt;
+    static boolean[][] check;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    
+    public int solution(int[][] land) {
+        int answer = 0;
+        map = new HashMap<>();
+        check = new boolean[land.length][land[0].length];
+        
+        int n = 2;
+        for(int i =0;i<land.length;i++) {
+            for(int j =0;j<land[0].length;j++) {
+                if(land[i][j] == 1 && !check[i][j]) {
+                    bfs(land,i,j,n);
+                    // map.put(n,cnt);
+                    n++;
+
+                }
+            }
+        }
+        
+//            for(int i =0;i<land.length;i++) {
+//             for(int j =0;j<land[0].length;j++) {
+//                 System.out.print(land[i][j]+" ");
+//             }
+//                 System.out.println();
+               
+//         }
+        
+//                         System.out.println(n);
+
+        
+        
+//         for(int y = 0; y<land[0].length;y++) {
+//             int tmp = -1;
+//             int count =0;
+//             for(int x = 0; x<land.length;x++) {
+//                 if(land[x][y] > 1 && tmp != land[x][y]) {
+//                     tmp = land[x][y];
+//                     count += map.get(land[x][y]);
+//                 }
+
+//             }
+//             max = Math.max(max,count);
+//         }
+        
+        int max = 0;
+        for(Integer count : map.values()) {
+            max = Math.max(max,count);
+            // System.out.print(count+" / ");
+        }
+        
+        
+        
+        return max;
+    }
+    
+    
+    
+    static void bfs(int[][] land,int x,int y,int n) {
+        
+        Set<Integer> set = new HashSet<>();
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x,y));
+        check[x][y] = true;
+        land[x][y] = n;
+        set.add(y);
+        cnt = 1;
+
+        
+        while(!q.isEmpty()) {
+            Point p = q.poll();
+            
+            for(int i =0;i<4;i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                
+                if(nx < 0 || nx >= land.length || ny < 0 || ny >= land[0].length) continue;
+                if(land[nx][ny] == 1 && !check[nx][ny]) {
+                    check[nx][ny] = true;
+                    land[nx][ny] = n;
+                    cnt++;
+                    set.add(ny);
+                    q.add(new Point(nx,ny));
+                }
+                
+            }
+        }
+        
+        for(int yy : set) {
+            map.put(yy,map.getOrDefault(yy,0)+cnt);
+        }
+        
+    }
+}
