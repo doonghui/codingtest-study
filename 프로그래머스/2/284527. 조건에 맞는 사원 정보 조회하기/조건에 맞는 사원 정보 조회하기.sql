3d55c75570918f9a89e1e6223b9f404871154f31
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-14 13:37:44 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,4 +1,2 @@
-select sum(gr.SCORE) as SCORE,gr.EMP_NO,emp.EMP_NAME,emp.POSITION,emp.EMAIL from HR_GRADE as gr join HR_EMPLOYEES as emp on gr.EMP_NO = emp.EMP_NO 
-group by gr.EMP_NO order by sum(gr.SCORE) desc limit 1;
-
-
+select  (select sum(score) from  HR_GRADE  group by EMP_NO order by sum(score) desc limit 1) as 'SCORE',em.EMP_NO,em.EMP_NAME,em.POSITION,em.EMAIL  from HR_EMPLOYEES em join HR_DEPARTMENT de on em.DEPT_ID = de.DEPT_ID 
+where em.EMP_NO = (select emp_no from HR_GRADE group by EMP_NO order by sum(score) desc limit 1)
