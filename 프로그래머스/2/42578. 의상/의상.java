23729bb7f8e89f9fe9cbb5bc5468ca52eba9738e
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-17 15:51:24 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,33 +3,28 @@ import java.util.*;
 class Solution {
 
     public  int solution(String[][] clothes) {
-          int answer = 0;
-
-        HashMap<String,Integer> map = new HashMap<>();
-
-        ArrayList<String> arr = new ArrayList<>();
-
+        int answer =1;
+        Map<String,Integer> map = new HashMap<>();
+        ArrayList<String> arr = new ArrayList<>();      // 종류
+        
         for(int i =0;i<clothes.length;i++) {
-            if(!map.containsKey(clothes[i][1])) {
-                map.put(clothes[i][1],1);
-                arr.add(clothes[i][1]);
-            } else {
-                map.put(clothes[i][1],map.get(clothes[i][1])+1);
-            }
+                if(map.containsKey(clothes[i][1])) {
+                    map.put(clothes[i][1],map.get(clothes[i][1])+1);
+                }
+                else {
+                    map.put(clothes[i][1],1);
+                    arr.add(clothes[i][1]);
+                }
         }
+        
+ //       map.forEach((key, value) -> {System.out.println(key+ " : " + value);        });
 
-
-        int value = 1;
-
-        for(int j =0;j<arr.size();j++) {
-            value *= (map.get(arr.get(j))+1);
+         for(int j =0;j<arr.size();j++) {
+        answer *= map.get(arr.get(j)) +1;
         }
-
-        answer = value -1;
-
-
-
-
+        
+        answer -= 1;
+        
         return answer;
 
 
