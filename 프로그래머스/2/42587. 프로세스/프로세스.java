86acd4d33b83ec408b3c9ebc2fe9089003bd8728
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-24 15:54:41 +0900

    [level 2] Title: 프로세스, Time: 2.14 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 class Priority {
-    int num;
+    int idx;
     int pri;
     
-    public Priority(int num,int pri) {
-        this.num = num;
+    public Priority(int idx, int pri) {
+        this.idx = idx;
         this.pri = pri;
     }
 }
@@ -14,29 +14,46 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        Queue<Priority> q = new LinkedList<>(); 
+        
+        ArrayList<Priority> arr = new ArrayList<>();
+        
         for(int i =0;i<priorities.length;i++) {
-            q.add(new Priority(i,priorities[i]));
-        }
+            arr.add(new Priority(i,priorities[i]));
+        } 
         
-        Arrays.sort(priorities);
         
-        boolean flag = false;
-        int idx = priorities.length-1;
         while(true) {
-            Priority p = q.poll();
-            
-            if(p.pri < priorities[idx]) {
-                q.add(p);
-            } else {
-                idx -=1;
-                answer++;
-                if(p.num == location) break;
+            Priority p = arr.get(0);
+            boolean flag = false;
+            for(int j = 1;j<arr.size();j++) {
+                if(arr.get(j).pri > p.pri) {
+                    flag = true;
+                    break;
+                }
             }
             
+            if(flag) {
+                arr.remove(0);
+                arr.add(p);
+            } else {
+                arr.remove(0);
+                answer++;
+            if(p.idx == location)
+                break;
+            }
+            
+            
+            
         }
         
         
+        // 1.hashMap
+        // 2. 
+        
+        // 맨 앞에 꺼내고
+        // 뒤에꺼랑 비교해보고 더 큰게 없으면 완전히 꺼낸다 
+        // 있으면 다시 뒤에 넣는다
+        // 만약 꺼낸 순서가 location 과 같다면 바로 return
         
         
         return answer;
