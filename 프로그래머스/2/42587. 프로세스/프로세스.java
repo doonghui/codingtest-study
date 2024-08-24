c6e7451dd18b11941e302c41572074662a96a526
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-24 16:09:09 +0900

    [level 2] Title: 프로세스, Time: 2.12 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,61 +1,42 @@
 import java.util.*;
 
-class Priority {
-    int idx;
-    int pri;
-    
-    public Priority(int idx, int pri) {
-        this.idx = idx;
-        this.pri = pri;
-    }
-}
+
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
         
-        ArrayList<Priority> arr = new ArrayList<>();
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());    // 내림차순
         
         for(int i =0;i<priorities.length;i++) {
-            arr.add(new Priority(i,priorities[i]));
+            pq.add(priorities[i]);
         } 
         
         
-        while(true) {
-            Priority p = arr.get(0);
-            boolean flag = false;
-            for(int j = 1;j<arr.size();j++) {
-                if(arr.get(j).pri > p.pri) {
-                    flag = true;
-                    break;
-                }
-            }
-            
-            if(flag) {
-                arr.remove(0);
-                arr.add(p);
-            } else {
-                arr.remove(0);
-                answer++;
-            if(p.idx == location)
-                break;
-            }
-            
-            
-            
-        }
+        boolean flag = false;
         
-        
-        // 1.hashMap
-        // 2. 
-        
-        // 맨 앞에 꺼내고
         // 뒤에꺼랑 비교해보고 더 큰게 없으면 완전히 꺼낸다 
         // 있으면 다시 뒤에 넣는다
         // 만약 꺼낸 순서가 location 과 같다면 바로 return
         
-        
+        while(!flag) {
+            for(int j = 0;j<priorities.length;j++) {
+                int pri = pq.poll();                            // 맨 앞에 꺼냄
+                if(priorities[j] == pri) {
+                    answer++;
+                    
+                    if(j == location) {flag = true;;
+                                      break;
+                                      }
+                } else {
+                    pq.add(pri);                                // 제일 큰게 아
+                }
+            }
+  
+        }
+
         return answer;
     }
 }
