63ef95fa3f6bc4764ec70f68180454007af13cbc
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-26 01:08:19 +0900

    [level 2] Title: 튜플, Time: 49.31 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,30 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String s) {
        
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        
-        for(int i =0;i<500;i++) arr.add(new ArrayList<>());         // return 하는 배열의 길이가 1 이상 500 이하인 경우
-        
-                String [] strs = s.replaceAll("[{}]", "").trim().split(",");
+  public int[] solution(String s) {
 
-        Arrays.sort(strs, (a,b)->(a.length()-b.length()));
-        
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 500; i++) arr.add(new ArrayList<>());         // return 하는 배열의 길이가 1 이상 500 이하인 경우
+
+        // String[] strs = s.replaceAll("[{}]", "").trim().split(",");
+  //      System.out.println(Arrays.toString(strs));
+
+        // Arrays.sort(strs, (a, b) -> (a.length() - b.length()));
+     //   System.out.println(Arrays.toString(strs));
+
         int j = 1;
         int idx = -1;
-        while(j < s.length()) {                                     // 배열로 바꾸기
-            if(s.charAt(j) != '{' && s.charAt(j) != ',' && s.charAt(j) != '}'){
+        while (j < s.length()) {                                     // 배열로 바꾸기
+            if (s.charAt(j) != '{' && s.charAt(j) != ',' && s.charAt(j) != '}') {
                 idx++;
                 int start = j;
                 int end = j;
-                while(true) {
+                while (true) {
                     end++;
-                    if(s.charAt(end) == ',') {
-                        arr.get(idx).add(Integer.parseInt(s.substring(start,end)));
-                        start = end+1;
-                    } else if(s.charAt(end) == '}') {
-                        arr.get(idx).add(Integer.parseInt(s.substring(start,end)));
+                    if (s.charAt(end) == ',') {
+                        arr.get(idx).add(Integer.parseInt(s.substring(start, end)));
+                        start = end + 1;
+                    } else if (s.charAt(end) == '}') {
+                        arr.get(idx).add(Integer.parseInt(s.substring(start, end)));
                         break;
                     }
                 }
@@ -35,29 +38,27 @@ class Solution {
         }
 
 
+        Collections.sort(arr, (a, b) -> (a.size() - b.size()));          // size 오름차순 정렬
 
-        
-        
-        Collections.sort(arr, (a,b)->(a.size()-b.size()));          // size 오름차순 정렬
-        
         HashSet<Integer> set = new HashSet<>();
         ArrayList<Integer> ans = new ArrayList<>();
 
-        for(ArrayList<Integer> ar : arr) {
-            if(ar.size() == 0) continue;
-            for(Integer in : ar) {
-                if(set.add(in)) {
+        for (ArrayList<Integer> ar : arr) {
+            if (ar.size() == 0) continue;
+            for (Integer in : ar) {
+                if (set.add(in)) {
                     ans.add(in);
                     break;
                 }
             }
         }
-         
+
         int[] answer = new int[ans.size()];
-        for(int l =0;l<ans.size();l++) {
+        for (int l = 0; l < ans.size(); l++) {
             answer[l] = ans.get(l);
         }
 
         return answer;
     }
 }
+
