fa3a77e977432c563f0068f904dacdefd6037e88
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-31 01:56:59 +0900

    [level 2] Title: 전화번호 목록, Time: 346.82 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);        // 사전순 정렬!
+        
+
+    
+  //  System.out.println(Arrays.toString(phone_book));
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String str= phone_book[i];
+            if(phone_book[i+1].length() < str.length()) continue;           // 문자열 사전순 정렬이라 앞에가 더 클 수 도 있음
+            if (phone_book[i+1].substring(0,str.length()).equals(str)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
