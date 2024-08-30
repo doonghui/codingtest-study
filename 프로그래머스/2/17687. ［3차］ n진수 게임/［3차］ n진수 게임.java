14cf04cc9a399ac8db26cd07d9b39003ef10a1e0
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-08-31 02:32:09 +0900

    [level 2] Title: [3차] n진수 게임, Time: 27.85 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,29 +1,6 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
-        
-        int num = 0; 
-        StringBuilder sb = new StringBuilder();
-        while(true) {
-            
-        String str = Integer.toString(num,n).toUpperCase();         // 진수 변환 + 소문자로 나와서 대문자로 변환
-        sb.append(str);
-        if(sb.length() > 100000) break;
-        num++;
-        }
-        
-        for(int i =0;i<t;i++) {
-            answer += sb.charAt(p-1 + m * i);
-
-            
-        }
-        
-
-        
-        
-        
         return answer;
     }
-    
-
 }
