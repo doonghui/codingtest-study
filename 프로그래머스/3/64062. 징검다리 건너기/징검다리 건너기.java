df7fccb998c25dbc204b38cfaf0b0d7b7fc3afaf
Donghui Park <62371491+doonghui@users.noreply.github.com>
2024-09-07 19:51:04 +0900

    [level 3] Title: 징검다리 건너기, Time: 28.36 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -2,8 +2,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] stones, int k) {
         int left = 1;
-        int right = 200000000;
-        int mid =  (left+right) /2;
+        int right = 200000000 + 200000;              // 건널 수 있는 인원 최대 2억 2십만..?
+        int mid =  (left+right) /2;         // 최대 갈수 있는 인원 수
         while(left <= right) {
             if(road(stones,mid,k)) {
                 left = mid +1;
@@ -26,7 +26,7 @@ class Solution {
 
     
     static boolean road(int[] stones, int friends, int k) {
-        int count = 0;
+        int count = 0;          // 한번에 건널 수 없는 디딤돌 수 카운트
         for (int j = 0; j < stones.length; j++) {
             if (stones[j] - friends < 0) count++;
             else count = 0;
