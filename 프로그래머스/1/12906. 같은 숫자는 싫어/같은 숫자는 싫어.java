4073e5d62cd631c17999601f5ac23f56688d5a4f
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2024-09-12 17:07:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.38 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+       
+        
+    
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        for(int i = 1; i<arr.length;i++) {
+            if(stack.peek() != arr[i]) stack.push(arr[i]);
+        }
+        
+        
+       int[] answer = new int[stack.size()];
+        
+        for(int j = answer.length-1;j>=0;j--) {
+
+            answer[j] = stack.pop();
+                }
+        
+        
+        
+        return answer;
+    }
+}
