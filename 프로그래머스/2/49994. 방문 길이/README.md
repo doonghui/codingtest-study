91ca1da8a5f229b514be95922f19f048aac24556
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2024-09-13 17:39:57 +0900

    [level 2] Title: 방문 길이, Time: 0.17 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 21.68 ms
+메모리: 75.6 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 14일 14:39:06
+2024년 09월 13일 17:39:49
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,65 +1,70 @@
+import java.util.*;
+
 class Solution {
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {-1,1,0,0};
-    static int[][][] map;
-    static int x;
-    static int y;
-    static int count =0;
+    
+    
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,1,-1};
     public int solution(String dirs) {
-        int answer = 0;
-        
-        // 처음 위치
-         x = 5;
-         y = 5;
-        map = new int[11][11][4];
-        
-        for(int i =0;i<dirs.length();i++) {
-            char c = dirs.charAt(i);
-            if(c == 'U') move(0);
-            else if(c == 'D') move(1);
-            else if(c == 'R') move(2);
-            else if(c == 'L') move(3);
-        }
         
         
-        answer = count;
+        // 내 중심을 5,5로 생각
+        int[][] map = new int[11][11];
+        boolean[][][] visited = new boolean[11][11][4];       // 상하좌우 판별
+        
+        int answer = solve(dirs,map,visited,5,5);
+        
+        
+        
         
         return answer;
     }
     
-    static void move(int idx) {
+    
+    static int solve(String dirs,int[][] map,boolean[][][] visited,int x,int y) {
+        int count = 0;
+        int idx = 0;
+        for(char c : dirs.toCharArray()) {
+            if(c == 'U') idx = 0;
+            else if(c == 'D') idx = 1;
+            else if(c == 'R') idx = 2; 
+            else if(c == 'L') idx = 3;
+             
         int nx = x + dx[idx];
         int ny = y + dy[idx];
+            
+        if(nx < 0 || nx > 10 || ny < 0 || ny > 10) continue;
         
-        System.out.print(nx+" "+ny+"=");
-        
-        if(nx < 0 || nx > 10 || ny < 0 || ny > 10) { // 범위 벗어날 때
-            System.out.print("nop! || ");
-            return;    }
-        
-               int reverseIdx = 0;
+            
+            
+            
+            int reverseIdx = 0;
             if(idx == 1 || idx == 3) {
                 reverseIdx = idx-1;
             } else {
                 reverseIdx = idx+1;
             }
-        
-        
-        if(map[nx][ny][idx] == 0 && map[x][y][reverseIdx] == 0) {
-     
-            count +=1;
-            map[nx][ny][idx] = 1;
-            map[x][y][reverseIdx] = 1;
-                        System.out.print("new || ");
-         
-        }else {
-            System.out.print("old || ");
-        }      
+            
+            if(!visited[x][y][idx] && !visited[nx][ny][reverseIdx] ) {
+            visited[x][y][idx] = true;
+            visited[nx][ny][reverseIdx] = true;
+            count++;
+           // System.out.print(c+" "); 
+            }
+      
+        // 다음칸으로 이동
         x = nx;
         y = ny;
+            
+    
+            
+            
+        }
         
         
         
+        
+    return count;
         
     }
 }
