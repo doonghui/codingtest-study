18c78ef924718e9db26d29c78a43ab74c9390680
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2024-09-16 17:39:21 +0900

    [level 2] Title: 주식가격, Time: 39.12 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,35 +1,27 @@
 import java.util.*;
-
-
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
-        int[] dp = new int[prices.length];    
+        int[] answer = new int[prices.length];
         
-    
-        for(int i = prices.length-1;i>=0;i--) {
-            int price = prices[i];
-            int num =0;               
+        // 테스트5 시간초과 남 -> 스택2개 이용했을 때.. 1개로 하면가능 다시 풀어야 함
+        Stack<Integer> stack = new Stack<>();       // 
 
-            for(int j = i+1;j<prices.length;j++) {
-                // System.out.print(i+" "+j+"");
-                num++;
-                if(price >prices[j]) {
-                    break;
-                }
-                    
+        for(int i = 0;i<prices.length;i++) {
+            answer[i] = prices.length-1-i;      // 가격이 떨어지지않을 때 최대 기간 
+            while(!stack.isEmpty()) {
+                if(prices[stack.peek()] <= prices[i]) break;        // 가격이 안떨어지면 그냥 나가기
+                
+                    int idx = stack.pop();
+                    answer[idx] =i - idx;
             }
             
-            dp[i] = num;
+            stack.push(i);
+        
         }
-        
-       //                System.out.println();
 
+ 
         
-//         for(int i : dp) {
-//             System.out.print(i+" ");
-//         }
-        
-        return dp;
+    //  System.out.println(stack.size());
+        return answer;
     }
 }
