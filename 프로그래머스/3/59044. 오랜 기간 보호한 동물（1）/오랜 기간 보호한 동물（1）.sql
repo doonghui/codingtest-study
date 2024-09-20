250177bcd48c168f4f13a2793c5e1b69ca28d7f8
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2024-09-21 00:00:47 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,5 @@
 select A.NAME, A.DATETIME from ANIMAL_INS A
-    left outer join ANIMAL_OUTS B
+left outer join ANIMAL_OUTS B
     on A.ANIMAL_ID = B.ANIMAL_ID
     where B.ANIMAL_ID is null
     order by A.DATETIME
