7d4ba4176c79c8500aba4352fc58e00f44bb7e5a
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2024-09-21 15:19:56 +0900

    [level 2] Title: 소수 찾기, Time: 42.94 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,57 @@
+
+
 import java.util.*;
 class Solution {
-  
-    static int ans = 0;
+       static int count =0;
     static boolean[] visited;
-
     public  int solution(String numbers) {
+        int answer = 0;
 
-        char[] c = numbers.toCharArray();
 
-        Set<Integer> set = new HashSet<>();     // 같은 수 제거 위해
-        visited = new boolean[c.length];
-        for (int i = 1; i <= c.length; i++) {
-
-            per(c, i, 0, "", set);
+        visited = new boolean[numbers.length()];
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 1;i<=numbers.length();i++) {
+            char[] dist = new char[i];
+            solve(numbers,0,i,dist,set);
 
         }
 
-        return ans;
+
+        return count;
     }
 
-    static void per(char[] c, int len, int L, String str, Set<Integer> set) {
-        if (L == len) {
-            int num = Integer.parseInt(str);
-            if (set.contains(num)) return;
+
+    static void solve(String numbers,int L,int len,char[] dist,HashSet<Integer> set) {
+        if(L == len) {
+            String s = "";
+            for(char str : dist) {
+                s += str;
+            }
+            int num = Integer.parseInt(s);
+            if(set.contains(num)) return;
             set.add(num);
-            if(num == 0 || num == 1) return;
-            for (int i = 2; i * i <= num; i++) {        // 소수 구하기
-                if (num % i == 0) return;
+            if(num == 1 || num == 0) return;
+            for(int i =2;i*i<=num;i++) {
+                if(num % i == 0) return;
             }
 
-            ans++;
+            System.out.println(num);
+            count++;
             return;
         }
 
-        for (int k = 0; k < c.length; k++) {
-            if (!visited[k]) {
-                str += c[k];
-                visited[k] = true;
-                per(c, len, L + 1, str, set);
-                visited[k] = false;
-              str = str.substring(0, str.length()-1);
-            }
+        for(int j = 0;j<numbers.length();j++) {
+            if(!visited[j]) {
+                visited[j] = true;
+                dist[L] = numbers.charAt(j);
+                solve(numbers,L+1,len,dist,set);
+                visited[j] = false;
 
+            }
 
         }
 
 
+
     }
-    
 }
