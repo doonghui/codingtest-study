5aabf7d385a0a03b8851e6ceb5adf591d749efaf
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2024-10-01 15:42:32 +0900

    [level 2] Title: 숫자 변환하기, Time: 319.12 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -4,6 +4,7 @@ class Solution {
         int answer = 0;
         
         HashSet<Integer> set= new HashSet<>();
+        Queue<int[]> q = new LinkedList<>();
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[]o2) {
@@ -11,24 +12,29 @@ class Solution {
                 return o1[0] - o2[0];
             }
             
+            // 5,7,9,10,11,14,15,16 틀림..
+            //     public int compare(int[] o1, int[]o2) {
+            //     if(o1[1] == o2[1]) return o1[0] - o2[0];
+            //     return o1[1] - o2[1];
+            // }
+            
         });
-        pq.add(new int[]{x,0});
-        while(!pq.isEmpty()) {
-            int[] num = pq.poll();
+        q.add(new int[]{x,0});
+        while(!q.isEmpty()) {
+            int[] num = q.poll();
             
             if(num[0] == y) return num[1];
             
-            if(num[0] > y) break;
+            if(num[0] > y) continue;
             
            if(!set.contains(num[0])) {
                set.add(num[0]); 
                
     
-              pq.add(new int[]{num[0]+n,num[1]+1});
-              pq.add(new int[]{num[0]*2,num[1]+1});
-              pq.add(new int[]{num[0]*3,num[1]+1});
+              q.add(new int[]{num[0]+n,num[1]+1});
+              q.add(new int[]{num[0]*2,num[1]+1});
+              q.add(new int[]{num[0]*3,num[1]+1});
                
-              
            }
             
         }
