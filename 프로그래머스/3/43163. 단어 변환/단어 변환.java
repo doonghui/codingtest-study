e572b2b9b41157415289d34f2b030877290c80d3
ParkDongHui <62371491+doonghui@users.noreply.github.com>
2025-03-21 10:23:52 +0900

    [level 3] Title: 단어 변환, Time: 0.77 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Node {
+    String word;
+    int count;
+
+    public Node(String word,int count) {
+        this.word = word;
+        this.count = count;
+
+    }
+
+
+}
+
+class Solution {
+
+    public int solution(String begin, String target, String[] words) {
+         int answer = 0;
+        boolean flag = false;
+        boolean[] visited = new boolean[words.length];      // 한번 방문한곳 다시 방문하지 않기 위해
+
+        // words 안에 target 이 있는지 확인
+        for (String str : words) {
+            if (target.equals(str)) flag = true;
+        }
+        if (!flag) return answer;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(begin,0));
+
+        while (!q.isEmpty()) {
+            Node nod = q.poll();
+            for (int i = 0; i < words.length; i++) {
+                int tmp = 0;
+                if (!visited[i]) {
+                    for (int j = 0; j < begin.length(); j++) {
+                        if (words[i].charAt(j) != nod.word.charAt(j)) tmp++;
+                    }
+                    if (tmp == 1) {
+                        visited[i] = true;
+                        if (words[i].equals(target)) return nod.count + 1;
+                        q.offer(new Node(words[i],nod.count+1));
+                    }
+                }
+
+            }
+        }
+
+
+        return answer;
+    }
+}
